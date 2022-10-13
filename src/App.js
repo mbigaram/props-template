e2933f6647330defb93085ec2cf9be97204810f4
mbiga <mbigaram.mb@gmail.com>
2022-10-13 11:50:48 -0300

    Exercicios pratica guiada

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+  const nome = "Ammal"
+  const nome2 = "Marcelo"
+  function apresentaGaragem(){
+    alert(`Boas vindas a garagem da ${nome}`);
+  }
+  function apresentaGaragem2(){
+    alert(`Boas vindas a garagem d0 ${nome2}`);
+  }
   return (
     <div className="App">
-      <Garagem />
+      <Garagem nome={nome}mensagemApres ={apresentaGaragem}/>
+      <Garagem nome={nome2}
+      mensagemApres = {apresentaGaragem2}/>
     </div>
   );
 }
